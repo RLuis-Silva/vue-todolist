68b778ce6881729856813ad8d4da77c44f9b79ab
Luis <s.ros.lui@gmail.com>
2020-11-10 16:45:19 +0100

    inizio js

---
+++ main.js
@@ -0,0 +1,15 @@
+// console.log("Js funziona!", Vue);
+
+const vue = new Vue({
+    el: '#vue',
+    data: {
+      toDoPred1  : 'Fare i compiti',
+      toDoPred2  : 'Fare la spesa',
+      toDoPred3  : 'Fare il bucato'
+    },
+
+    methods: {
+        // cliccando il bottone 'aggiungi' la funzione aggiunge un To-do alla lista
+
+    }
+});
