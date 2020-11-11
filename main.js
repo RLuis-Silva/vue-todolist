21d22f42cf2d3707ab1397a81489db07c1465416
Luis <s.ros.lui@gmail.com>
2020-11-11 14:05:14 +0100

    add funzione aggiungiTodo()

--- main.js
+++ main.js
@@ -7,15 +7,19 @@ const vue = new Vue({
     //   toDoPred2  : 'Fare la spesa',
     //   toDoPred3  : 'Fare il bucato'
 
-      toDoPreds: [
+      toDos: [
         'Fare i compiti',
         'Fare la spesa',
         'Fare il bucato'
-      ]
+      ],
+
+      newToDo: ''
     },
 
     methods: {
         // cliccando il bottone 'aggiungi' la funzione aggiunge un To-do alla lista
-
+        aggiungiTodo(){
+            this.toDos.push(this.newToDo);
+        }
     }
 });
